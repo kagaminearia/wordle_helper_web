03bfbb5e10edc3247b53e5aa2ac8754c7df2bc49
kagaminearia <rinkitang@outlook.com>
2022-05-09 00:45:33 -0400

    add host

--- wordle_img/settings.py
+++ wordle_img/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = 'django-insecure-)dxg(q9vhb_hmf4zke7#)qt5dw)+6$n8#ami!!7ow++k**5u#p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    'fierce-depths-69391.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
